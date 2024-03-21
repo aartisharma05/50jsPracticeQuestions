26609381f9c31483364639347929721fce1c7ea9
aartisharma05 <aartisharma.official1@gmail.com>
2024-03-21 21:47:46 +0530

    2

---
+++ 02.js
@@ -0,0 +1,21 @@
+//print numbers from 1 to 5 to the console using loop
+
+console.log("using for loop");
+for (var i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+console.log("using while loop");
+var i = 1;
+while (i <= 5) {
+  console.log(i);
+  i++;
+}
+
+console.log("using do while loop");
+
+let j = 1;
+do {
+  console.log(j);
+  j++;
+} while (j <= 5);
