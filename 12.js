ac91db00da6027b85d7bd082821f307407a1762d
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-03-28 23:09:14 +0530

    Create 12.js

---
+++ 12.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/array-reduce-transformation/description/
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    if(nums.length === 0){
+        return init;
+    }
+    
+    for(var i =0;i<nums.length;i++){
+        init = fn(init, nums[i]);
+    }
+    return init
+};
