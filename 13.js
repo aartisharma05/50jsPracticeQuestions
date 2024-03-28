0bdb000abaedb556d715d009df98b6caad53814f
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-03-28 23:09:53 +0530

    Create 13.js

---
+++ 13.js
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/filter-elements-from-array/description/
+
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var filter = function(arr, fn) {
+    const filteredArr = [];
+    for(var i =0;i<arr.length;i++){
+        if(fn(arr[i], i)){
+            filteredArr.push(arr[i])
+        }
+    }
+
+    return filteredArr;
+};
