e182c9198d23349cc019c393d477fbe9aae00b66
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-03-29 23:58:37 +0530

    Create 19.js

---
+++ 19.js
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/add-two-promises/description/
+
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+  let sum = await promise1 + await promise2;
+  return sum;
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
