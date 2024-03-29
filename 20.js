012cd40be246b608d81b80734d5be5b7fad1664e
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-03-29 23:59:15 +0530

    Create 20.js

---
+++ 20.js
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/array-prototype-last/description/
+
+/**
+ * @return {null|boolean|number|string|Array|Object}
+ */
+Array.prototype.last = function() {
+    if(this.length>0) {return this[this.length-1];}
+    else return -1;
+};
+
+/**
+ * const arr = [1, 2, 3];
+ * arr.last(); // 3
+ */
