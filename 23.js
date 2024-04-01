0d6b7e37edde83ec0d5b99555873f4c838f52fd4
AARTI SHARMA <aartisharma.official1@gmail.com>
2024-04-01 23:53:12 +0530

    Valid Anagram

---
+++ 23.js
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/valid-anagram/
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    s = s.split("").sort().join("");
+    t = t.split("").sort().join("");
+
+    return s===t;
+};
